6504cc084b6d20fd38bd3c0b1bad56f7ef831c6f
seaofbytes <sandilukez990@gmail.com>
2021-08-01 23:54:04 +0200

    file upload

--- src/components/NewPost.svelte
+++ src/components/NewPost.svelte
@@ -7,6 +7,17 @@
   // Push Notifications
   const { addNotification } = getNotificationsContext();
 
+  let avatar, fileinput;
+
+  const onFileSelected = (e) => {
+    let image = e.target.files[0];
+    let reader = new FileReader();
+    reader.readAsDataURL(image);
+    reader.onload = (e) => {
+      avatar = e.target.result;
+    };
+  };
+
   // New post form
   const { form, errors, state, handleChange, handleSubmit } = createForm({
     initialValues: {
@@ -40,20 +51,41 @@
       <h1 class="text-center text-2xl">New post</h1>
 
       <form on:submit={handleSubmit} class="pt-6 pb-2 my-2">
-        <div class="mb-6">
-          <!-- password -->
-          <label class="block text-sm font-bold mb-2" for="password"> Image </label>
-          <input
-            on:change={handleChange}
-            bind:value={$form.image_url}
-            class="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker mb-3"
-            id="image_url"
-            type="text"
-            placeholder="Image url"
-          />
-          {#if $errors.image_url}
-            <small class="uppercase">{$errors.image_url}</small>
+        <div id="app">
+          <h1>Upload Image</h1>
+
+          {#if avatar}
+            <img class="avatar" src={avatar} alt="d" />
+          {:else}
+            <img
+              class="avatar"
+              src="https://cdn4.iconfinder.com/data/icons/small-n-flat/24/user-alt-512.png"
+              alt=""
+            />
           {/if}
+          <img
+            class="upload"
+            src="https://static.thenounproject.com/png/625182-200.png"
+            alt=""
+            on:click={() => {
+              fileinput.click();
+            }}
+          />
+          <div
+            class="chan"
+            on:click={() => {
+              fileinput.click();
+            }}
+          >
+            Choose Image
+          </div>
+          <input
+            style="display:none"
+            type="file"
+            accept=".jpg, .jpeg, .png"
+            on:change={(e) => onFileSelected(e)}
+            bind:this={fileinput}
+          />
         </div>
 
         <div class="mb-4">
@@ -88,3 +120,24 @@
     </div>
   </div>
 </div>
+
+<style>
+  #app {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-flow: column;
+  }
+
+  .upload {
+    display: flex;
+    height: 50px;
+    width: 50px;
+    cursor: pointer;
+  }
+  .avatar {
+    display: flex;
+    height: 200px;
+    width: 200px;
+  }
+</style>
